c30795362bf04cdafdc3d68f9306ed316e5a3503
Sam Shum <dev@ahxshum.com>
2024-08-08 13:27:20 +0100

    feat: add dispatcher to app state

--- src/app/state.rs
+++ src/app/state.rs
@@ -1,13 +1,17 @@
-use crate::database::{Database, Query};
+use crate::{
+    database::{Database, Query},
+    taskqueue::{Dispatcher, Task},
+};
 
 #[derive(Clone)]
 pub struct AppState {
     db: Database,
+    dispatcher: Dispatcher,
 }
 
 impl AppState {
-    pub fn new(db: Database) -> Self {
-        Self { db }
+    pub fn new(db: Database, dispatcher: Dispatcher) -> Self {
+        Self { db, dispatcher }
     }
 
     pub fn database(&self) -> &Database {
@@ -16,5 +20,9 @@ impl AppState {
 
     pub fn new_query(&self) -> Query {
         Query::new()
+    }
+
+    pub fn dispatcher(&self) -> &Dispatcher {
+        &self.dispatcher
     }
 }

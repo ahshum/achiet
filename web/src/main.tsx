f6127bbb68a8b9a92e316b7b483df87e9982554d
Sam Shum <dev@ahxshum.com>
2024-12-07 19:54:28 +0000

    feat: enable scss

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
-import "./index.css"
+import "./index.scss"
 import { createBrowserRouter, RouterProvider } from "react-router-dom"
 import Root from "./routes/Root.tsx"
 import Login from "./routes/Login.tsx"
